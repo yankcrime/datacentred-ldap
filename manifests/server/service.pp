ee8a67850f34c87dbb7a130bea4c7b6bd1b7a1f8
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-02-02 10:00:46 +0100

    Add ability to set service flags for OpenBSD. This is necessary, to actually tell the server to listen on LDAPs or only listen on localhost. Defaults to undef.
    exaple Hiera entry to tell it to listen only on localhost, plain
    ldap as well as ldaps:
    
    ldap::server::service_flags: '-u _openldap -h ldap://localhost\ ldaps://localhost'
    
    Guard the usage of the 'flags' service parameter with a
    if statement, testing for ::osfamily == 'OpenBSD'
    The 'flags' parameter got added with Puppet 3.6.X and
    with Ruby versions < 2.X it also seems to be problematic
    as the spec tests show

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -4,12 +4,26 @@
 #
 class ldap::server::service inherits ldap::server {
   if $ldap::server::service_manage == true {
-    service { 'ldap-server':
-      ensure     => $ldap::server::service_ensure,
-      enable     => $ldap::server::service_enable,
-      name       => $ldap::server::service_name,
-      hasstatus  => true,
-      hasrestart => true,
+    # Puppet versions prior 3.6 did not have the
+    # flags parameter, and Ruby versions < 2 seem to
+    # be problematic in general with that parameter
+    if $::osfamily == 'OpenBSD' {
+      service { 'ldap-server':
+        ensure     => $ldap::server::service_ensure,
+        enable     => $ldap::server::service_enable,
+        name       => $ldap::server::service_name,
+        flags      => $ldap::server::service_flags,
+        hasstatus  => true,
+        hasrestart => true,
+      }
+    } else {
+      service { 'ldap-server':
+        ensure     => $ldap::server::service_ensure,
+        enable     => $ldap::server::service_enable,
+        name       => $ldap::server::service_name,
+        hasstatus  => true,
+        hasrestart => true,
+      }
     }
   }
 }
